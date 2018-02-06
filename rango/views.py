c64da5122620235455c0dc2f127e3551c14be8cd
Andrew Ritchie <2253409r@student.gla.ac.uk>
2018-02-06 20:16:06 +0000

    end of chapter 10

--- rango/views.py
+++ rango/views.py
@@ -10,10 +10,12 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 
 
 def index(request):
+        request.session.set_test_cookie()
         # Query the database for a list of ALL categories currently stored.
         # Order the categories by no. likes in descending order.
         # Retrieve the top 5 only - or all if less than 5.
@@ -23,12 +25,23 @@ def index(request):
         category_list = Category.objects.order_by('-likes')[:5]
         page_list = Page.objects.order_by('-views')[:5]
         context_dict = {'categories': category_list, 'pages': page_list}
+
+        # Call the helper function to handle the cookies
+        visitor_cookie_handler(request)
+        context_dict['visits'] = request.session['visits']
+        
+        # Obtain our Response object early so we can add cookie information.
+        response = render(request, 'rango/index.html', context_dict)
+
+
+        # Return response back to the user, updating any cookies that need changed.
+        return response
         
 
-        # Render the response and send it back!
-        return render(request, 'rango/index.html', context_dict)
-
 def about(request):
+        if request.session.test_cookie_worked():
+                print("TEST COOKIE WORKED!")
+                request.session.delete_test_cookie()
         # Construct a dictionary to pass to the template engine as its context.
         # Note the key boldmessage is the same as {{ boldmessage }} in the template!
         context_dict = {'boldmessage': "This tutorial has been put together by Andrew Ritchie"}
@@ -144,7 +157,8 @@ def register(request):
                         # Now sort out the UserProfile instance.
                         # Since we need to set the user attribute ourselves,
                         # we set commit=False. This delays saving the model
-                        # until we're ready to avoid integrity problems.
+                        # until we're ready to avoid integrity problems.
+
                         profile = profile_form.save(commit=False)
                         profile.user = user
 
@@ -230,5 +244,34 @@ def user_logout(request):
         # Since we know the user is logged in, we can now just log them out.
         logout(request)
         # Take the user back to the homepage.
-        return HttpResponseRedirect(reverse('index'))
+        return HttpResponseRedirect(reverse('index'))
 
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+        val = request.session.get(cookie)
+        if not val:
+                val = default_val
+        return val
+
+
+# Updated the function definition
+def visitor_cookie_handler(request):
+        
+        visits = int(get_server_side_cookie(request, 'visits', '1'))
+        last_visit_cookie = get_server_side_cookie(request,
+                                                        'last_visit',
+                                                        str(datetime.now()))
+        last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                                '%Y-%m-%d %H:%M:%S')
+
+        # If it's been more than a day since the last visit...
+        if (datetime.now() - last_visit_time).days > 0:
+                visits = visits + 1
+                #update the last visit cookie now that we have updated the count
+                request.session['last_visit'] = str(datetime.now())
+        else:
+                # set the last visit cookie
+                request.session['last_visit'] = last_visit_cookie
+
+        # Update/set the visits cookie
+        request.session['visits'] = visits
